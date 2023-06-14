a54a954fa0a2e2a13d44f8423131631532bbb4a2
ant <athai6@gatech.edu>
2023-06-14 14:11:35 -0400

    clean up

--- data_generation/rendering/render_toys.sh
+++ data_generation/rendering/render_toys.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 data_path='/data/DevLearning/toys4k_blend_files'
-output_path='/home/ant/develop/CRIBpp_generic/toys_rendering_output_l3'
-blender_path='/home/ant/Downloads/blender-2.91.2-linux64/blender'
-scene_config_path='/home/ant/develop/CRIBpp_generic/common/toys_scene_configs_l3_test'
+output_path='/home/ant/develop/LSME/data_generation/toys_rendering_output'
+blender_path='/home/ant/Downloads/blender-2.93.7-linux-x64/blender'
+scene_config_path='/home/ant/develop/LSME/data_generation/common/toys_scene_configs'
 
 python wrapper.py \
     --start=0 \
